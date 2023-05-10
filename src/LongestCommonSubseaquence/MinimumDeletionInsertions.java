1b3c62e92f5d526da90b978f75423efb7e003361
sravanKumar <sravankumarnc@gmail.com>
2023-05-10 11:37:57 +0530

    Practice Problems

---
+++ src/LongestCommonSubseaquence/MinimumDeletionInsertions.java
@@ -0,0 +1,27 @@
+package LongestCommonSubseaquence;
+
+public class MinimumDeletionInsertions {
+    public int minOperations(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int dp[][] = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return m + n - 2 * dp[m][n];
+    }
+}
